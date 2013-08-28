59647365e75d7a02502db7cb37b1539e6feeb484
Paulo Dalaneca <piratadown2@gmail.com>
2013-08-27 23:40:04 -0300

    Primeiro commit

---
+++ index.py
@@ -0,0 +1,10 @@
+#!/Python23/python
+print 'Content-type: text/html'
+print
+print '<html>'
+print '<head>'
+print '<title>Olá mundo</title>'
+print '</head>'
+print '<body>'
+print '</body>'
+print '</html>'
